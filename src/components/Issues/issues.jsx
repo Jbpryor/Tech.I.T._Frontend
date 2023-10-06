7755ee82d590b100dbb52a75d86eb570e8640a3b
Jason Pryor <125523612+Jbpryor@users.noreply.github.com>
2023-10-05 23:26:05 -0500

    sort function for issues

--- src/components/Issues/issues.jsx
+++ src/components/Issues/issues.jsx
@@ -1,31 +1,115 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './issues.scss'
 import { useLocation, Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import 'boxicons/css/boxicons.min.css';
 
 function Issues({ projectIssues }) {
     const location = useLocation();
     const isIssuesActive = location.pathname === '/issues';
     const issues = useSelector((state) => state.issues)
-    const sortedIssues = [...issues].sort((a, b) => {
-        const priorityOrder = ['Critical', 'High', 'Medium', 'Low'];
-        const priorityA = priorityOrder.indexOf(a.priority);
-        const priorityB = priorityOrder.indexOf(b.priority);
-        if (priorityA !== priorityB) {
-            return priorityA - priorityB;
-        }
-        return a.created.localeCompare(b.created);
-    });
+
+    function sortByProperty(property, customOrder = null, reverse = false) {
+        return (a, b) => {
+            const aValue = a[property];
+            const bValue = b[property];
     
+            if (property === 'created') {
+                const aDate = new Date(aValue);
+                const bDate = new Date(bValue);
+    
+                if (aDate < bDate) return reverse ? 1 : -1;
+                if (aDate > bDate) return reverse ? -1 : 1;
+                return 0;
+            }
+    
+            if (customOrder) {
+                const aIndex = customOrder.indexOf(aValue);
+                const bIndex = customOrder.indexOf(bValue);
+                return reverse ? bIndex - aIndex : aIndex - bIndex;
+            }
+    
+            return reverse ? bValue.localeCompare(aValue) : aValue.localeCompare(bValue);
+        };
+    };
+
+    const priorityOrder = ['Critical', 'High', 'Medium', 'Low'];
+    const statusOrder = ['Open', 'In Progress', 'Under Review', 'Resolved', 'Postponed', 'Closed'];
+
     const isProjectsActive = /^\/projects\//.test(location.pathname);
     
-    const filteredIssues = isProjectsActive ? projectIssues : sortedIssues;
+    const filteredIssues = isProjectsActive ? projectIssues : issues;
+
+    const sortedIssuesByDate = [...filteredIssues].sort(sortByProperty('created', null, true));
+    const sortedIssuesByDeveloper = [...filteredIssues].sort(sortByProperty('developer', null, true));
+    const sortedIssuesByPriority = [...filteredIssues].sort(sortByProperty('priority', priorityOrder, true));
+    const sortedIssuesByProject = [...filteredIssues].sort(sortByProperty('project', null, true));
+    const sortedIssuesByStatus = [...filteredIssues].sort(sortByProperty('status', statusOrder, true));
+    const sortedIssuesBySubmitter = [...filteredIssues].sort(sortByProperty('submitter', null, true));
+    const sortedIssuesByTitle = [...filteredIssues].sort(sortByProperty('title', null, true));
+    const sortedIssuesByType = [...filteredIssues].sort(sortByProperty('type', null, true));
+
+
+    const [ rotate, setRotate ] = useState(false);
+    const [ selectedSort, setSelectedSort ] = useState('');
+
+    const handleRotate = () => {
+        setRotate(!rotate);
+        setSortOrder(sortOrder === 'ascending' ? 'descending' : 'ascending')
+    }
+
+    const handleSort = () => {
+
+    }
+
+    const [sortOrder, setSortOrder] = useState('ascending');
+
+    const getSortingFunction = () => {
+        switch (selectedSort) {
+            case 'Date':
+                return sortOrder === 'ascending' ? sortedIssuesByDate : sortedIssuesByDate.reverse();
+            case 'Developer':
+                return sortOrder === 'ascending' ? sortedIssuesByDeveloper : sortedIssuesByDeveloper.reverse();
+            case 'Priority':
+                return sortOrder === 'ascending' ? sortedIssuesByPriority : sortedIssuesByPriority.reverse();
+            case 'Project':
+                return sortOrder === 'ascending' ? sortedIssuesByProject : sortedIssuesByProject.reverse();
+            case 'Status':
+                return sortOrder === 'ascending' ? sortedIssuesByStatus : sortedIssuesByStatus.reverse();
+            case 'Submitter':
+                return sortOrder === 'ascending' ? sortedIssuesBySubmitter : sortedIssuesBySubmitter.reverse();
+            case 'Title':
+                return sortOrder === 'ascending' ? sortedIssuesByTitle : sortedIssuesByTitle.reverse();
+            case 'Type':
+                return sortOrder === 'ascending' ? sortedIssuesByType : sortedIssuesByType.reverse();
+            default:
+                return sortOrder === 'ascending' ? sortedIssuesByDate : sortedIssuesByDate.reverse();
+        }
+      };
+      
+      
+    const sortedIssues = getSortingFunction();      
     
     return (
         <section className="issues">
             <div className={`issues-title ${isIssuesActive ? 'active' : ''}`}>Issues</div>
+
+            <div className="issues-sort-container" value={selectedSort} onChange={(event) => setSelectedSort(event.target.value)}>
+                <select className='issues-sort-select'>
+                    <option>Date</option>
+                    <option>Developer</option>
+                    <option>Priority</option>
+                    <option className={`project-select ${isProjectsActive ? 'active' : ''}`}>Project</option>
+                    <option>Status</option>
+                    <option>Submitter</option>
+                    <option>Title</option>
+                    <option>Type</option>
+                </select>
+                <i className={`bx bx-down-arrow issues-sort-icon ${rotate ? 'rotate' : ''}`} onClick={() => { handleRotate(); handleSort() }}></i>
+            </div>
+
             <div className={`issues-container ${isIssuesActive ? 'active' : ''}`}>              
-            {filteredIssues.map((issue) => (
+            {sortedIssues.map((issue) => (
                 <Link className='issue-link' to={`/issues/${issue.id}`} key={issue.id}>
                     <div className={`issue-container ${isIssuesActive ? 'active' : ''}`}>       
                         <div className="issue-title">{issue.title}</div>

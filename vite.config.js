bfc9ac41f5926569939ec2f0754c826a7aa1d98d
Jason Pryor <125523612+Jbpryor@users.noreply.github.com>
2024-02-01 10:31:32 -0600

    change base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Tech.I.T._Frontend",
+  base: "/",
   build: {
     outDir: 'dist',
     chunkSizeWarningLimit: 1000,

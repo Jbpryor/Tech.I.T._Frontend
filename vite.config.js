702b92fd82beb9c2da415fbe8bcc7e2945eba92d
Jason Pryor <125523612+Jbpryor@users.noreply.github.com>
2024-02-01 09:01:17 -0600

    changed vite url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Tech.I.T.",
+  base: "/Tech.I.T._Frontend",
   build: {
     outDir: 'docs',
   },

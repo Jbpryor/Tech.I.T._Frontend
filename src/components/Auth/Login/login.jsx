b10b2ab127a810403c94f33c10283240d22d18e1
Jason Pryor <125523612+Jbpryor@users.noreply.github.com>
2024-02-15 22:54:46 -0600

    update countdown timer

--- src/components/Auth/Login/login.jsx
+++ src/components/Auth/Login/login.jsx
@@ -6,7 +6,6 @@ import { useNavigate, NavLink } from "react-router-dom";
 import { login } from "../authApiSlice";
 import { setCredentials } from "../authSlice";
 import { PulseLoader } from "react-spinners";
-import useCountdownTimer from "../../../Hooks/useCountdownTimer";
 
 const EMAIL_REGEX = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/;
 const PWD_REGEX = /^[A-z0-9!@#$%]{4,12}$/;
@@ -16,13 +15,14 @@ function Login() {
   const errRef = useRef();
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const conuntdown = useCountdownTimer(60);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [demoUser, setDemoUser] = useState(false);
   const [errMsg, setErrMsg] = useState("");
   const [validEmail, setValidEmail] = useState(false);
   const [validPassword, setValidPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [countdown, setCountdown] = useState(60);
 
   const handleDemoUser = () => {
     setEmail("demo@example.com");
@@ -30,7 +30,17 @@ function Login() {
     setDemoUser(true);
   };
 
-  const [isLoading, setIsLoading] = useState(false);
+  const startCountdown = () => {
+    const countdownInterval = setInterval(() => {
+      setCountdown((prevCountdown) => {
+        if (prevCountdown === 0) {
+          clearInterval(countdownInterval);
+          return 0;
+        }
+        return prevCountdown - 1;
+      });
+    }, 1000);
+  };
 
   useEffect(() => {
     setErrMsg("");
@@ -50,6 +60,7 @@ function Login() {
     event.preventDefault();
 
     setIsLoading(true);
+    startCountdown();
 
     const response = await dispatch(login({ email, password }));
 
@@ -92,7 +103,7 @@ function Login() {
   if (isLoading)
     return (
       <section className="login-container">
-        ...Loading ({conuntdown} seconds)
+        {`...Loading (${countdown} seconds)`}
         <PulseLoader color={"#FFF"} />
       </section>
     );
